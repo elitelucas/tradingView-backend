85b9fcfb7f7b5e5bbc740c33d51bb50f825daa3d
vladimir9255 <vovochkaperepelkin@yandex.ru>
2021-02-19 10:48:54 +0900

    Update tradingController.js

--- src/controller/tradingController.js
+++ src/controller/tradingController.js
@@ -23,10 +23,10 @@ module.exports = {
         params.range=req.query.from ? (new Date(req.query.from*1000)).getFullYear() : 2021;
         console.log(params);
         const result = await d.query(`
-        SELECT  * from "ADMIN".":table"
-        WHERE TRUNC(DATETIME)>=TO_DATE('01/JAN/2021','dd/mon/yyyy')
+        SELECT  * from "ADMIN"."GBPUSD_1M"
+        WHERE TRUNC(DATETIME)>=TO_DATE('01/JAN/:range','dd/mon/yyyy')
         `        
-        , [params.table]);
+        , [params.range]);
         // const result = await d.query(`
         // SELECT  * from "ADMIN"."GBPUSD_1M"
         // WHERE TRUNC(DATETIME)>=TO_DATE('01/JAN/2021','dd/mon/yyyy')
