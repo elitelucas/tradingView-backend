7efb641eb2ad09b23144910a74e7af43b9f26d79
vladimir9255 <vovochkaperepelkin@yandex.ru>
2021-02-19 11:25:11 +0900

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const routes = require('./routes.js')
-
+var cors = require('cors');
 const app = express();
 app.use(express.json());
-
+app.use(cors());
 app.use('/api', routes);
 
 app.listen(3000);
